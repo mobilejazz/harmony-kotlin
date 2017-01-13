4eb0e8b5a419c1c036488aabc2e461b5b2cfa4e2
Jose Luis  Franconetti Olmedo <joseluis@mobilejazz.com>
2017-01-13 11:41:29 +0100

    Moved @PerActivity annotation to the worldreader core

---
+++ core/src/main/java/com/worldreader/core/application/di/annotation/PerActivity.java
@@ -0,0 +1,10 @@
+package com.worldreader.core.application.di.annotation;
+
+import javax.inject.Scope;
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Scope @Retention(RUNTIME) public @interface PerActivity {
+
+}
