a424ef618e734bb0a881fad31324dd7b2ab9a336
Jose Luis  Franconetti Olmedo <joseluis@mobilejazz.com>
2017-02-16 16:26:13 +0100

    Implemented offline userbook datasource

--- core/src/main/java/com/worldreader/core/datasource/storage/datasource/cache/manager/table/UserBooksTable.java
+++ core/src/main/java/com/worldreader/core/datasource/storage/datasource/cache/manager/table/UserBooksTable.java
@@ -18,6 +18,7 @@ public class UserBooksTable {
   public static final String COLUMN_SAVED_OFFLINE_AT = "saveOfflineAt";
   public static final String COLUMN_RATING = "rating";
   public static final String COLUMN_LIKED = "liked";
+  public static final String COLUMN_SYNC = "syncronization";
   public static final String COLUMN_COLLECTION_IDS = "collectionIds";
   public static final String COLUMN_CREATED_AT = "createdAt";
   public static final String COLUMN_UPDATED_AT = "updatedAt";
@@ -50,7 +51,7 @@ public class UserBooksTable {
         + " TEXT, "
         + COLUMN_RATING
         + " INTEGER DEFAULT 0, "
-        + COLUMN_LIKED
+        + COLUMN_LIKED + " INTEGER DEFAULT 0, " + COLUMN_SYNC
         + " INTEGER DEFAULT 0, "
         + COLUMN_CREATED_AT
         + " TEXT NOT NULL DEFAULT (STRFTIME('%Y-%m-%dT%H:%M:%SZ', 'now')), "
