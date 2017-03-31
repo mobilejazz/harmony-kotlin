1791ddee52e8b929728a618c43b592a7fd860112
Jose Luis  Franconetti Olmedo <joseluis@mobilejazz.com>
2017-03-31 10:41:59 +0200

    Removed logs from the WorldreaderBooksServerInterceptor interceptor.

--- core/src/main/java/com/worldreader/core/datasource/network/general/retrofit/interceptor/WorldreaderBooksServerInterceptor.java
+++ core/src/main/java/com/worldreader/core/datasource/network/general/retrofit/interceptor/WorldreaderBooksServerInterceptor.java
@@ -60,12 +60,12 @@ public class WorldreaderBooksServerInterceptor implements Interceptor {
     }
 
     long t1 = System.nanoTime();
-    logger.d(TAG, String.format("Sending request %s on %s%n%s", request.url(), chain.connection(), request.headers()));
+    //logger.d(TAG, String.format("Sending request %s on %s%n%s", request.url(), chain.connection(), request.headers()));
 
     Response response = chain.proceed(request);
 
     long t2 = System.nanoTime();
-    logger.d(TAG, String.format("Received response for %s in %.1fms%n%s", response.request().url(), (t2 - t1) / 1e6d, response.headers()));
+    //logger.d(TAG, String.format("Received response for %s in %.1fms%n%s", response.request().url(), (t2 - t1) / 1e6d, response.headers()));
 
     return response;
   }
