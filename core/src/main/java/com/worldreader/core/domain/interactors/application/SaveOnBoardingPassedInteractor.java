71aa40a763291ad4b8d37c4a07be26a213c6adc9
Aldo Borrero González <aldo@aldoborrero.com>
2017-06-29 12:35:06 +0200

    Removed deprecated method in SaveOnBoardingPassedInteractor

--- core/src/main/java/com/worldreader/core/domain/interactors/application/SaveOnBoardingPassedInteractor.java
+++ core/src/main/java/com/worldreader/core/domain/interactors/application/SaveOnBoardingPassedInteractor.java
@@ -1,14 +1,9 @@
 package com.worldreader.core.domain.interactors.application;
 
 import com.google.common.util.concurrent.ListenableFuture;
-import com.worldreader.core.common.deprecated.error.ErrorCore;
-import com.worldreader.core.domain.deprecated.DomainCallback;
-
-import java.util.concurrent.*;
+import java.util.concurrent.Executor;
 
 public interface SaveOnBoardingPassedInteractor {
-
-  void execute(boolean status, DomainCallback<Boolean, ErrorCore> callback);
 
   ListenableFuture<Boolean> execute(final boolean status);
 
