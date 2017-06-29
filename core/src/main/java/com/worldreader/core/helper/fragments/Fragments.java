c8fff1b3cc19618e64dc60cf1df0840005dc2f73
Aldo Borrero González <aldo@aldoborrero.com>
2017-06-29 20:06:28 +0200

    Updated fragments class

--- core/src/main/java/com/worldreader/core/helper/fragments/Fragments.java
+++ core/src/main/java/com/worldreader/core/helper/fragments/Fragments.java
@@ -1,6 +1,7 @@
 package com.worldreader.core.helper.fragments;
 
 import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -11,48 +12,45 @@ import android.support.v4.app.FragmentManager;
  */
 @SuppressWarnings("unused") public class Fragments {
 
-  public static void replaceNowAllowingStateLoss(final FragmentActivity activity, final Fragment f,
-      @IdRes final int container) {
+  private Fragments() {
+    throw new AssertionError("No instances of this class are allowed!");
+  }
+
+  public static void replaceNowAllowingStateLoss(final FragmentActivity activity, final Fragment f, @IdRes final int container) {
     try {
       replaceNow(activity, f, container);
     } catch (IllegalStateException e) {
-      activity.getSupportFragmentManager()
-          .beginTransaction()
-          .replace(container, f)
-          .commitNowAllowingStateLoss();
+      activity.getSupportFragmentManager().beginTransaction().replace(container, f).commitNowAllowingStateLoss();
     }
   }
 
-  public static void replaceNow(final FragmentActivity activity, final Fragment f,
-      @IdRes final int container) {
+  public static void replaceNow(final FragmentActivity activity, final Fragment f, @IdRes final int container) {
     replaceNow(activity.getSupportFragmentManager(), f, container);
   }
 
-  public static void replaceNowAllowingStateLoss(final FragmentActivity activity, final Fragment f,
-      @IdRes final int container, final String tag) {
+  public static void replaceNowAllowingStateLoss(final FragmentActivity activity, final Fragment f, @IdRes final int container, final String tag) {
     try {
       replaceNow(activity, f, container, tag);
     } catch (IllegalStateException e) {
-      activity.getSupportFragmentManager()
-          .beginTransaction()
-          .replace(container, f, tag)
-          .commitNowAllowingStateLoss();
+      activity.getSupportFragmentManager().beginTransaction().replace(container, f, tag).commitNowAllowingStateLoss();
     }
   }
 
-  public static void replaceNow(final FragmentActivity activity, final Fragment f,
-      @IdRes final int container, final String tag) {
+  public static void replaceNow(final FragmentActivity activity, final Fragment f, @IdRes final int container, final String tag) {
     replaceNow(activity.getSupportFragmentManager(), f, container, tag);
   }
 
-  public static void replaceNow(final FragmentManager manager, final Fragment f,
-      @IdRes final int container) {
+  public static void replaceNow(final FragmentManager manager, final Fragment f, @IdRes final int container) {
     manager.beginTransaction().replace(container, f).commitNow();
   }
 
-  public static void replaceNow(final FragmentManager manager, final Fragment f,
-      @IdRes final int container, final String tag) {
+  public static void replaceNow(final FragmentManager manager, final Fragment f, @IdRes final int container, final String tag) {
     manager.beginTransaction().replace(container, f, tag).commitNow();
+  }
+
+  public static void replace(@NonNull final FragmentActivity activity, @NonNull final Fragment f, @IdRes final int container) {
+    final FragmentManager fm = activity.getSupportFragmentManager();
+    fm.beginTransaction().replace(container, f).commit();
   }
 
   @Nullable public static Fragment findByTag(final FragmentActivity activity, final String tag) {
@@ -77,10 +75,6 @@ import android.support.v4.app.FragmentManager;
 
   public static void showNow(final FragmentManager manager, final Fragment f) {
     manager.beginTransaction().show(f).commitNow();
-  }
-
-  private Fragments() {
-    throw new AssertionError("No instances of this class are allowed!");
   }
 
 }
