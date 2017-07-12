99a0fa3a5712866c9d0d77989c0af83b3215b981
Aldo Borrero González <aldo@aldoborrero.com>
2017-07-12 15:48:49 +0200

    Added pages and bookId fields to UserScore, UserScoreEntity, UserScoreTable and UserScoreDb

--- core/src/main/java/com/worldreader/core/datasource/storage/datasource/cache/manager/table/UserScoreTable.java
+++ core/src/main/java/com/worldreader/core/datasource/storage/datasource/cache/manager/table/UserScoreTable.java
@@ -7,8 +7,10 @@ public class UserScoreTable {
   public static final String TABLE = "userscore";
 
   public static final String COLUMN_USER_ID = "userId";
+  public static final String COLUMN_BOOK_ID = "bookId";
   public static final String COLUMN_SCORE_ID = "scoreId";
   public static final String COLUMN_SCORE = "score";
+  public static final String COLUMN_PAGES = "pages";
   public static final String COLUMN_SYNCHRONIZED = "synchronized";
   public static final String COLUMN_CREATED_AT = "createdAt";
   public static final String COLUMN_UPDATED_AT = "updatedAt";
@@ -19,10 +21,14 @@ public class UserScoreTable {
         + "("
         + COLUMN_USER_ID
         + " TEXT NOT NULL, "
+        + COLUMN_BOOK_ID
+        + " TEXT, "
         + COLUMN_SCORE_ID
         + " INTEGER PRIMARY KEY AUTOINCREMENT, "
         + COLUMN_SCORE
         + " INTEGER NOT NULL, "
+        + COLUMN_PAGES
+        + " INTEGER NOT NULL DEFAULT 0, "
         + COLUMN_SYNCHRONIZED
         + " INTEGER DEFAULT 0, "
         + COLUMN_CREATED_AT
