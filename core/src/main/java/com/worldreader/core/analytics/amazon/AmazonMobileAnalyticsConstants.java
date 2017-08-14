bf066573cdd3aa187af4719eef4e78ed72f8b6da
Flavia Moroso <flavia@worldreader.org>
2017-08-14 12:00:11 +0200

    Merge branch 'feature/AmazonMobileAnalytics_AD-253' of https://github.com/Worldreader/worldreader-android into feature/AmazonMobileAnalytics_AD-253
    # Conflicts:
    #	app/src/main/java/com/worldreader/analytics/AmazonMobileAnalyticsConstants.java

--- core/src/main/java/com/worldreader/core/analytics/amazon/AmazonMobileAnalyticsConstants.java
+++ core/src/main/java/com/worldreader/core/analytics/amazon/AmazonMobileAnalyticsConstants.java
@@ -11,12 +11,10 @@ public final class AmazonMobileAnalyticsConstants {
   public static final String STARTING_BOOK_DOWNLOAD_EVENT= "StartingBookDownload";
   public static final String BOOK_EXCEEDED_DOWNLOAD_LIMIT_EVENT= "ExceededDownloadLimit";
   public static final String BOOK_DELETE_DOWNLOADED_BOOK_EVENT= "DeleteDownload";
-  public static final String BOOK_ADD_COLLECTION_TO_LIBRARY_EVENT = "AddCollectionToLibrary";
+  public static final String ADD_COLLECTION_TO_LIBRARY_EVENT = "AddCollectionToLibrary";
+  public static final String REMOVE_COLLECTION_FROM_LIBRARY_EVENT = "RemoveCollectionFromLibrary";
   public static final String MORE_BOOKS_EVENT = "More";
   public static final String LOAD_MORE_BOOKS_EVENT = "LoadMore";//Scrolling down the list of books
-  public static final String MORE_BOOKS_LIBRARY_EVENT = "More_Books_Library";
-  public static final String MY_BOOKS_EVENT="MyBooks";
-  public static final String TIPS_EVENT = "Tips";
   public static final String COLLECTIONS_EVENT = "Collections";
   public static final String CATEGORIES_EVENT = "Categories";
   public static final String CATEGORY_EVENT = "CategoryDetails";
@@ -24,8 +22,9 @@ public final class AmazonMobileAnalyticsConstants {
   public static final String CATEGORY_SELECTION_EVENT = "CategorySelection";
   public static final String CATEGORY_SELECTED_ATTRIBUTE = "CategorySelected";
   public static final String PARENT_CATEGORY_ATTRIBUTE = "ParentCategory";
-  public static final String PARENT_CATEGORY_TITLE = "ParentCategoryTitle";
+  public static final String PARENT_CATEGORY_TITLE_ATTRIBUTE = "ParentCategoryTitle";
 
+  //Kenya LEAP Program events
   public static final String LOCAL_LIBRARY_SELECTED_EVENT = "LocalLibrarySelected";
   public static final String LOCAL_LIBRARY_NAME_ATTRIBUTE = "LocalLibraryName";
   public static final String LOCAL_LIBRARY_UUID_ATTRIBUTE = "LocalLibraryUuid";
@@ -40,49 +39,21 @@ public final class AmazonMobileAnalyticsConstants {
   public static final String SAVE_GOALS_EVENT = "SaveGoals";
   public static final String PAGES_PER_DAY_ATTRIBUTE = "PagesPerDayGoal";
   public static final String BOOK_FINISHED_EVENT = "BookFinished";
-
-  public static final String RAW_CONTROLLER = "Controller";
-  public static final String RC_ACCOUNT = "Account";
-  public static final String RC_AUTHOR = "Author";
-  public static final String RC_BOOK = "Book";
-  public static final String RC_CATEGORY = "Category";
-  public static final String RC_COLLECTION = "Collection";
-  public static final String RC_HOME = "Home";
-  public static final String RC_LIBRARY = "Library";
-  public static final String RC_MANAGE = "Manage";
-  public static final String RC_PROGRESS = "Progress";
-  public static final String RC_SEARCH = "Search";
-  public static final String RC_SHARE = "Share";
-
-  public static final String RAW_ACTION = "Action";
-
-  public static final String RA_LOGIN = "Login";//Controller:Account Action:Login
-  public static final String RA_LOGOFF = "LogOff";//Controller:Account Action:LogOff
-  public static final String RA_CHANGE_FONT_SIZE = "ChangeFontSize";//Controller:Account Action:ChangeFontSize
-  public static final String RA_REGISTER = "Register";//Controller:Account Action:Register
-  public static final String RA_FORGOT_PASSWORD = "ForgotPassword";//Controller:Account Action:ForgotPassword
-  public static final String RA_SET_CULTURE = "SetCulture";//Controller:Home Action:SetCulture
+  public static final String READ_IN_LANGUAGE_EVENT = "ReadInLanguage";
+  public static final String LANGUAGE_ISO3_ATTRIBUTE = "Language";
+  public static final String LANGUAGE_NAME_ATTRIBUTE = "LanguageName";
+  public static final String SEE_LEADER_BOAD_EVENT = "LeaderBoard";
+  public static final String LEADER_BOAD_PERIOD_ATTRIBUTE = "LeaderPeriod";
+  public static final String SEE_MILESTONES_EVENT ="SeeMilestones";
+  public static final String MILESTONES_AS_USERTYPE_ATTRIBUTE ="AsUserType";
+  public static final String MILESTONE_CLICKED_EVENT = "MilestoneClicked";
+  public static final String MILESTONE_ATTRIBUTE ="Milestone";
+  public static final String MILESTONE_STATE_ATTRIBUTE = "MilestoneState";
+  public static final String MILESTONE_DESC_ATTRIBUTE ="MilestoneDesc";
+  public static final String SHARE_BADGE_EVENT = "ShareBadge";
 
 
-  public static final String RA_DETAILS = "Details"; //Controller: Book, Action: Details, Controller: Category, Action: Details
-  public static final String RA_BOOKS = "Books"; //Controller: Category, Action: Books -> Event: More
-  public static final String RA_EMAIL_SHARE = "EmailShare"; //Controller:Book Action:EmailShare
-  public static final String RA_LIKE = "Like"; //Controller:Book Action:Like
-  public static final String RA_SAVE = "Save"; //Controller:Book Action:Save -> Event:BookDownload
-  public static final String RA_DELETE_SAVED = "DeleteSavedBook"; //Controller:Book Action:Save ->Event:DeleteDowload
-  public static final String RA_TOC= "TableOfContents"; //Controller:Book Action:TableOfContents
-  public static final String RA_UNLIKE = "UnLike"; //Controller:Book Action:Unlike
-  public static final String RA_ADD_USER_BOOK = "AddUserBook";//Controller: Library, Action:AddUserBook ->Event:AddUserBook
-  public static final String RA_REMOVE_USER_BOOK = "RemoveUserBook";//Controller: Library, Action:RemoveUserBook ->Event:RemoveUserBook
-  public static final String RA_TOGGLE_BOOK_FAVORITE = "ToggleBookFavorite"; //Controller: Library, Action:ToggleBookFavorite
-  public static final String RA_MANAGE_CHANGE_PASSWORD = "ChangePassword"; //Controller: Manage, Action: ChangePassword
-
-  public static final String RA_SHARE_WA="WhatsApp";//Controller:Share Action:WhatsApp
-  public static final String RA_TIPS = "Tips";//Controller: Together, Action:Tips
-  public static final String RA_REQUEST = "Request";//Controller: Serach,  Action: Request
-  public static final String RA_RESULTS = "Req_Results"; //Controller: Search, Action: Results
-  public static final String RA_RESULTS_CLICK = "ResultClick";
-  public static final String SEARCH_RESULT_SIZE ="SearchResultsSize";
+  public static final String SEARCH_RESULT_SIZE_ATTRIBUTE ="SearchResultsSize";
 
   public static final String BOOK_ATTRIBUTE = "BookId";
   public static final String BOOK_VERSION_ATTRIBUTE = "BookVersion";
