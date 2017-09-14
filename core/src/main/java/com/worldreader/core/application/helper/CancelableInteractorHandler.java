d23185d1edfbd9c9c6adadd1ffd96a5839333a63
fjmontiel <fjmontiel@spiralapps.com>
2017-09-14 19:10:21 +0200

    Add a CancelableFutureCallback and CancelableInteractorHandler to avoid having to check view nullity

---
+++ core/src/main/java/com/worldreader/core/application/helper/CancelableInteractorHandler.java
@@ -0,0 +1,56 @@
+package com.worldreader.core.application.helper;
+
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.worldreader.core.application.di.annotation.PerActivity;
+import com.worldreader.core.domain.thread.MainThread;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.Executor;
+
+import javax.inject.Inject;
+
+@PerActivity
+public class CancelableInteractorHandler {
+
+  private List<WeakReference<CancelableFutureCallback<?>>> callbacks;
+  private final MainThread mainThread;
+
+  @Inject
+  public CancelableInteractorHandler(MainThread mainThread) {
+    this.mainThread = mainThread;
+    this.callbacks = new ArrayList<>();
+  }
+
+  public <T> void addCallbackMainThread(ListenableFuture<T> listenableFuture, CancelableFutureCallback<T> callback) {
+    callbacks.add(new WeakReference<CancelableFutureCallback<?>>(callback));
+    Futures.addCallback(listenableFuture, callback, mainThread.getMainThreadExecutor());
+  }
+
+  public <T> void addCallback(ListenableFuture<T> listenableFuture, CancelableFutureCallback<T> callback) {
+    callbacks.add(new WeakReference<CancelableFutureCallback<?>>(callback));
+    Futures.addCallback(listenableFuture, callback);
+  }
+
+  public <T> void addCallback(ListenableFuture<T> listenableFuture, CancelableFutureCallback<T> callback, Executor executor) {
+    callbacks.add(new WeakReference<CancelableFutureCallback<?>>(callback));
+    Futures.addCallback(listenableFuture, callback, executor);
+  }
+
+  public void cancel() {
+    final Iterator<WeakReference<CancelableFutureCallback<?>>> iterator = callbacks.iterator();
+
+    while (iterator.hasNext()) {
+      final CancelableFutureCallback callback = iterator.next().get();
+      if (callback != null) {
+        callback.cancel();
+      } else {
+        iterator.remove();
+      }
+    }
+  }
+
+}
