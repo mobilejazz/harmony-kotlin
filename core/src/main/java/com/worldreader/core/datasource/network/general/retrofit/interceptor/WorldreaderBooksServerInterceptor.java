542faaca60e236193604c1c83bda7c308ffb36df
Aldo Borrero González <aldo@aldoborrero.com>
2017-09-24 19:14:46 +0200

    Simplified logic and unused resources from WorldreaderBooksServerInterceptor

--- core/src/main/java/com/worldreader/core/datasource/network/general/retrofit/interceptor/WorldreaderBooksServerInterceptor.java
+++ core/src/main/java/com/worldreader/core/datasource/network/general/retrofit/interceptor/WorldreaderBooksServerInterceptor.java
@@ -1,7 +1,6 @@
 package com.worldreader.core.datasource.network.general.retrofit.interceptor;
 
 import android.support.annotation.NonNull;
-import com.mobilejazz.logger.library.Logger;
 import com.worldreader.core.application.di.qualifiers.WorldreaderBookApiEndpointToken;
 import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
@@ -9,7 +8,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 import javax.inject.Inject;
-import java.io.IOException;
+import java.io.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -24,14 +23,12 @@ public class WorldreaderBooksServerInterceptor implements Interceptor {
   private static final String WORLDREADER_ANDROID_CLIENT = "org.worldreader.wrms.android/1.0.0.0";
 
   private final String token;
-  private final  Logger logger;
 
-  @Inject public WorldreaderBooksServerInterceptor(final @WorldreaderBookApiEndpointToken String token, Logger logger) {
+  @Inject public WorldreaderBooksServerInterceptor(final @WorldreaderBookApiEndpointToken String token) {
     this.token = token;
-    this.logger = logger;
   }
 
-  @Override public Response intercept(Chain chain) throws IOException {
+  @Override public Response intercept(@NonNull Chain chain) throws IOException {
     Request request = chain.request();
 
     //Build new request
@@ -59,21 +56,14 @@ public class WorldreaderBooksServerInterceptor implements Interceptor {
       e.printStackTrace();
     }
 
-    long t1 = System.nanoTime();
-    //logger.d(TAG, String.format("Sending request %s on %s%n%s", request.url(), chain.connection(), request.headers()));
-
-    Response response = chain.proceed(request);
-
-    long t2 = System.nanoTime();
-    //logger.d(TAG, String.format("Received response for %s in %.1fms%n%s", response.request().url(), (t2 - t1) / 1e6d, response.headers()));
-
-    return response;
+    return chain.proceed(request);
   }
 
   /**
    * Generate the full request path
    *
    * @param httpUrl - {@link HttpUrl} Object to get all the parts of the request
+   *
    * @return Full path request
    */
   @NonNull private String buildPath(HttpUrl httpUrl) {
@@ -105,17 +95,12 @@ public class WorldreaderBooksServerInterceptor implements Interceptor {
    *
    * @param httpUrl - {@link HttpUrl} Object to get all the parts of the request
    * @param timestamp - It's a requirement of the API
+   *
    * @return Value to make the hash
    */
   @NonNull private String buildStringToHash(HttpUrl httpUrl, String timestamp) {
-    String path = buildPath(httpUrl);
-
-    StringBuilder builder = new StringBuilder();
-    builder.append(path);
-    builder.append(timestamp);
-    builder.append(token);
-
-    return builder.toString();
+    final String path = buildPath(httpUrl);
+    return path + timestamp + token;
   }
 
   /**
@@ -123,24 +108,26 @@ public class WorldreaderBooksServerInterceptor implements Interceptor {
    *
    * @param httpUrl - {@link HttpUrl} Object to get all the parts of the request
    * @param timestamp - It's a requirement of the API
+   *
    * @return Request hash
+   *
    * @throws NoSuchAlgorithmException
    */
   private String buildRequestHash(HttpUrl httpUrl, String timestamp) throws NoSuchAlgorithmException {
-    String valueToHash = buildStringToHash(httpUrl, timestamp);
+    final String valueToHash = buildStringToHash(httpUrl, timestamp);
     return sha256(valueToHash);
   }
 
   private String sha256(String value) throws NoSuchAlgorithmException {
-    MessageDigest md = MessageDigest.getInstance("SHA-256");
+    final MessageDigest md = MessageDigest.getInstance("SHA-256");
     md.update(value.getBytes());
 
     byte byteData[] = md.digest();
 
     //convert the byte to hex format method 1
-    StringBuffer sb = new StringBuffer();
-    for (int i = 0; i < byteData.length; i++) {
-      sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+    final StringBuilder sb = new StringBuilder();
+    for (byte aByteData : byteData) {
+      sb.append(Integer.toString((aByteData & 0xff) + 0x100, 16).substring(1));
     }
 
     return sb.toString();
