0c2599da07ef88f9b3373950df50b8eed7b8f664
fjmontiel <fjmontiel@spiralapps.com>
2017-09-26 10:51:28 +0200

    Change bandwidth limits on ImageResourceQualityProvider

--- core/src/main/java/com/worldreader/core/datasource/network/quality/ImageResourceQualityProvider.java
+++ core/src/main/java/com/worldreader/core/datasource/network/quality/ImageResourceQualityProvider.java
@@ -5,8 +5,8 @@ import javax.inject.Singleton;
 
 @Singleton public final class ImageResourceQualityProvider {
 
-  private static final double LOW_QUALITY_DOWNLOAD_RATE_THRESHOLD = 200;
-  private static final double MEDIUM_QUALITY_DOWNLOAD_RATE_THRESHOLD = 1000;
+  private static final double LOW_QUALITY_DOWNLOAD_RATE_THRESHOLD = 150;
+  private static final double MEDIUM_QUALITY_DOWNLOAD_RATE_THRESHOLD = 2000;
 
   private static final ImageResourceQuality DEFAULT_QUALITY = ImageResourceQuality.MEDIUM;
 
