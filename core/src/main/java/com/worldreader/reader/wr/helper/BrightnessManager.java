a20dc7a749e0b3fbdb2a1f56adc7d30f62cea342
Aldo Borrero González <aldo@aldoborrero.com>
2017-11-13 14:45:44 +0100

    Simplified BrigthnessManager class

--- core/src/main/java/com/worldreader/reader/wr/helper/BrightnessManager.java
+++ core/src/main/java/com/worldreader/reader/wr/helper/BrightnessManager.java
@@ -9,19 +9,11 @@ public class BrightnessManager {
   }
 
   public void setBrightness(Window window, int level) {
-    setBrightnessForManualMode(window, level);
-  }
-
-  private void setBrightnessForManualMode(Window window, int level) {
-    setBrightnessForWindow(window, level);
-  }
-
-  private void setBrightnessForWindow(Window window, int level) {
     if (window != null) {
       WindowManager.LayoutParams lp = window.getAttributes();
       lp.screenBrightness = (float) level / 100f;
       window.setAttributes(lp);
     }
-
   }
+
 }
