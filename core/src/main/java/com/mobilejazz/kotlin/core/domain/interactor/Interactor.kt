470e77ee3178e2f8e4647a0fca5e0043d058886f
Jose Luis  Franconetti Olmedo <joseluis@mobilejazz.com>
2018-04-30 12:48:19 +0200

    Added all the changes to the Repository

---
+++ core/src/main/java/com/mobilejazz/kotlin/core/domain/interactor/Interactor.kt
@@ -0,0 +1,47 @@
+package com.mobilejazz.kotlin.core.domain.interactor
+
+import com.mobilejazz.kotlin.core.datasource.*
+import com.mobilejazz.kotlin.core.threading.AppExecutor
+import java.util.concurrent.Callable
+
+class GetObjectInteractor<M> constructor(private val executor: AppExecutor, private val getRepository: GetRepository<M>) {
+
+    operator fun invoke(query: Query, operation: Operation = DefaultOperation()): Future<M> = executor.submit(Callable {
+        getRepository.get(query, operation).get()
+    })
+}
+
+class GetAllObjectsInteractor<M> constructor(private val executor: AppExecutor, private val getRepository: GetRepository<M>) {
+
+    operator fun invoke(query: Query, operation: Operation = DefaultOperation()): Future<List<M>> = executor.submit(Callable {
+        getRepository.getAll(query, operation).get()
+    })
+}
+
+class PutObjectInteractor<M> constructor(private val executor: AppExecutor, private val putRepository: PutRepository<M>) {
+
+    operator fun invoke(m: M, query: Query, operation: Operation = DefaultOperation()): Future<M> = executor.submit(Callable {
+        putRepository.put(query, m, operation).get()
+    })
+}
+
+class PutAllObjectsInteractor<M> constructor(private val executor: AppExecutor, private val putRepository: PutRepository<M>) {
+
+    operator fun invoke(m: List<M>, query: Query, operation: Operation = DefaultOperation()): Future<List<M>> = executor.submit(Callable {
+        putRepository.putAll(query, m, operation).get()
+    })
+}
+
+class DeleteObjectInteractor constructor(private val executor: AppExecutor, private val deleteRepository: DeleteRepository) {
+
+    operator fun invoke(query: Query, operation: Operation = DefaultOperation()): Future<Void> = executor.submit(Callable {
+        deleteRepository.delete(query, operation).get()
+    })
+}
+
+class DeleteAllObjectsInteractor constructor(private val executor: AppExecutor, private val deleteRepository: DeleteRepository) {
+
+    operator fun invoke(query: Query, operation: Operation = DefaultOperation()): Future<Void> = executor.submit(Callable {
+        deleteRepository.deleteAll(query, operation).get()
+    })
+}
