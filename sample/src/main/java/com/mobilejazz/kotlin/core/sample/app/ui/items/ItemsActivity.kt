8c1f3d31ae12199113abe8f57bbcd8600f5c7be6
Fran Montiel <furanu@gmail.com>
2018-05-16 19:39:18 +0200

    Merge branch 'master' of bitbucket-mobilejazz:mobilejazz/mj-android-core2
    # Conflicts:
    #	core/build.gradle
    #	sample/build.gradle
    #	sample/src/main/java/com/mobilejazz/kotlin/core/sample/app/ui/items/ItemsActivity.kt
    #	sample/src/main/java/com/mobilejazz/kotlin/core/sample/domain/items/GetItemsInteractor.kt
    #	sample/src/main/java/com/mobilejazz/kotlin/core/sample/domain/model/Item.kt
    #	sample/src/main/java/com/mobilejazz/kotlin/core/sample/repository/entity/ItemEntity.kt
    #	sample/src/main/java/com/mobilejazz/kotlin/core/sample/repository/network/ItemNetworkDataSource.kt

--- sample/src/main/java/com/mobilejazz/kotlin/core/sample/app/ui/items/ItemsActivity.kt
+++ sample/src/main/java/com/mobilejazz/kotlin/core/sample/app/ui/items/ItemsActivity.kt
@@ -2,50 +2,46 @@ package com.mobilejazz.kotlin.core.sample.app.ui.items
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import com.mobilejazz.kotlin.core.repository.*
-import com.mobilejazz.kotlin.core.repository.datasource.DeleteDataSource
-import com.mobilejazz.kotlin.core.repository.datasource.GetDataSource
-import com.mobilejazz.kotlin.core.repository.datasource.PutDataSource
-import com.mobilejazz.kotlin.core.repository.mapper.Mapper
 import com.mobilejazz.kotlin.core.sample.R
+import com.mobilejazz.kotlin.core.sample.R.id.items_lv
 import com.mobilejazz.kotlin.core.sample.domain.model.Item
 import com.mobilejazz.kotlin.core.ui.base.view.BaseMVPActivity
 import kotlinx.android.synthetic.main.activity_items.*
 
 
-fun main(args: Array<String>) {
-    val itemNetworkDataSource = ItemNetworkDataSource()
-
-    val getNetworkDatasource: GetDataSource<ItemEntity> = itemNetworkDataSource
-    val putNetworkDataSource: PutDataSource<ItemEntity> = itemNetworkDataSource
-    val deleteNetworkDataSource: DeleteDataSource = itemNetworkDataSource
-
-    val itemStorageDataSource = InMemoryDataSource<ItemEntity>()
-
-    val getStorageDataSource: GetDataSource<ItemEntity> = itemStorageDataSource
-    val putStorageDataSource: PutDataSource<ItemEntity> = itemStorageDataSource
-    val deleteStorageDataSource: DeleteDataSource = itemStorageDataSource
-
-    val itemNetworkStorageRepository = NetworkStorageRepository(getStorageDataSource, putStorageDataSource, deleteStorageDataSource, getNetworkDatasource, putNetworkDataSource, deleteNetworkDataSource)
-
-    val toItemEntityMapper = ItemToItemEntityMapper()
-    val toItemMapper = ItemEntityToItemMapper()
-
-    val itemRepositoryMapper = RepositoryMapper(itemNetworkStorageRepository, itemNetworkStorageRepository, itemNetworkStorageRepository, toItemEntityMapper, toItemMapper)
-
-    val getRepository: GetRepository<Item> = itemRepositoryMapper
-    val putRepository: PutRepository<Item> = itemRepositoryMapper
-    val deleteRepository: DeleteRepository = itemRepositoryMapper
-
-
-    val getAllItem = GetAllInteractor(AppExecutor, getRepository)
-
-    getAllItem(operation = StorageSyncOperation).onCompleteUi(onSuccess = {
-        Log.d("TEST", it.toString())
-    }, onFailure = {
-        Log.d("TEST", "Error")
-    })
-}
+//fun main(args: Array<String>) {
+//    val itemNetworkDataSource = ItemNetworkDataSource()
+//
+//    val getNetworkDatasource: GetDataSource<ItemEntity> = itemNetworkDataSource
+//    val putNetworkDataSource: PutDataSource<ItemEntity> = itemNetworkDataSource
+//    val deleteNetworkDataSource: DeleteDataSource = itemNetworkDataSource
+//
+//    val itemStorageDataSource = InMemoryDataSource<ItemEntity>()
+//
+//    val getStorageDataSource: GetDataSource<ItemEntity> = itemStorageDataSource
+//    val putStorageDataSource: PutDataSource<ItemEntity> = itemStorageDataSource
+//    val deleteStorageDataSource: DeleteDataSource = itemStorageDataSource
+//
+//    val itemNetworkStorageRepository = NetworkStorageRepository(getStorageDataSource, putStorageDataSource, deleteStorageDataSource, getNetworkDatasource, putNetworkDataSource, deleteNetworkDataSource)
+//
+//    val toItemEntityMapper = ItemToItemEntityMapper()
+//    val toItemMapper = ItemEntityToItemMapper()
+//
+//    val itemRepositoryMapper = RepositoryMapper(itemNetworkStorageRepository, itemNetworkStorageRepository, itemNetworkStorageRepository, toItemEntityMapper, toItemMapper)
+//
+//    val getRepository: GetRepository<Item> = itemRepositoryMapper
+//    val putRepository: PutRepository<Item> = itemRepositoryMapper
+//    val deleteRepository: DeleteRepository = itemRepositoryMapper
+//
+//
+//    val getAllItem = GetAllInteractor(AppExecutor, getRepository)
+//
+//    getAllItem(operation = StorageSyncOperation).onCompleteUi(onSuccess = {
+//        Log.d("TEST", it.toString())
+//    }, onFailure = {
+//        Log.d("TEST", "Error")
+//    })
+//}
 
 class ItemsActivity : BaseMVPActivity<ItemsPresenter, ItemsPresenter.View>(), ItemsPresenter.View {
 
@@ -54,48 +50,48 @@ class ItemsActivity : BaseMVPActivity<ItemsPresenter, ItemsPresenter.View>(), It
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_items)
 
-        val itemNetworkDataSource = ItemNetworkDataSource()
-
-        val getNetworkDatasource: GetDataSource<ItemEntity> = itemNetworkDataSource
-        val putNetworkDataSource: PutDataSource<ItemEntity> = itemNetworkDataSource
-        val deleteNetworkDataSource: DeleteDataSource = itemNetworkDataSource
-
-        val itemStorageDataSource = InMemoryDataSource<ItemEntity>()
-
-        val getStorageDataSource: GetDataSource<ItemEntity> = itemStorageDataSource
-        val putStorageDataSource: PutDataSource<ItemEntity> = itemStorageDataSource
-        val deleteStorageDataSource: DeleteDataSource = itemStorageDataSource
-
-        val itemNetworkStorageRepository = NetworkStorageRepository(getStorageDataSource, putStorageDataSource, deleteStorageDataSource, getNetworkDatasource, putNetworkDataSource, deleteNetworkDataSource)
-
-        val toItemEntityMapper = ItemToItemEntityMapper()
-        val toItemMapper = ItemEntityToItemMapper()
-
-        val itemRepositoryMapper = RepositoryMapper(itemNetworkStorageRepository, itemNetworkStorageRepository, itemNetworkStorageRepository, toItemEntityMapper, toItemMapper)
-
-        val getRepository: GetRepository<Item> = itemRepositoryMapper
-        val putRepository: PutRepository<Item> = itemRepositoryMapper
-        val deleteRepository: DeleteRepository = itemRepositoryMapper
-
-
-        val getAllItem = GetAllInteractor(AppExecutor, getRepository)
-
-        getAllItem(query = StringKeyQuery("all-items"), operation = StorageSyncOperation).onCompleteUi(onSuccess = {
-            Log.d("TEST", it.toString())
-        }, onFailure = {
-            Log.d("TEST", "Error")
-        })
-
-
-    }
-
-    override fun onDisplayItems(items: List<Item>) {
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items.map { it.toString() })
-        items_lv.adapter = ItemsAdapter(items)
-    }
+//        val itemNetworkDataSource = ItemNetworkDataSource()
+//
+//        val getNetworkDatasource: GetDataSource<ItemEntity> = itemNetworkDataSource
+//        val putNetworkDataSource: PutDataSource<ItemEntity> = itemNetworkDataSource
+//        val deleteNetworkDataSource: DeleteDataSource = itemNetworkDataSource
+//
+//        val itemStorageDataSource = InMemoryDataSource<ItemEntity>()
+//
+//        val getStorageDataSource: GetDataSource<ItemEntity> = itemStorageDataSource
+//        val putStorageDataSource: PutDataSource<ItemEntity> = itemStorageDataSource
+//        val deleteStorageDataSource: DeleteDataSource = itemStorageDataSource
+//
+//        val itemNetworkStorageRepository = NetworkStorageRepository(getStorageDataSource, putStorageDataSource, deleteStorageDataSource, getNetworkDatasource, putNetworkDataSource, deleteNetworkDataSource)
+//
+//        val toItemEntityMapper = ItemToItemEntityMapper()
+//        val toItemMapper = ItemEntityToItemMapper()
+//
+//        val itemRepositoryMapper = RepositoryMapper(itemNetworkStorageRepository, itemNetworkStorageRepository, itemNetworkStorageRepository, toItemEntityMapper, toItemMapper)
+//
+//        val getRepository: GetRepository<Item> = itemRepositoryMapper
+//        val putRepository: PutRepository<Item> = itemRepositoryMapper
+//        val deleteRepository: DeleteRepository = itemRepositoryMapper
+//
+//
+//        val getAllItem = GetAllInteractor(AppExecutor, getRepository)
+//
+//        getAllItem(query = StringKeyQuery("all-items"), operation = StorageSyncOperation).onCompleteUi(onSuccess = {
+//            Log.d("TEST", it.toString())
+//        }, onFailure = {
+//            Log.d("TEST", "Error")
+//        })
 
 
-    override fun onDisplayError(throwable: Throwable) {
+  }
 
-    }
+  override fun onDisplayItems(items: List<Item>) {
+    val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items.map { it.toString() })
+    items_lv.adapter = ItemsAdapter(items)
+  }
+
+
+  override fun onDisplayError(throwable: Throwable) {
+
+  }
 }
