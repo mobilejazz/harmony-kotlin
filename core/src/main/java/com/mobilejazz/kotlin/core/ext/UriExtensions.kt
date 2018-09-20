fdea817f599c5d41d894ca2ae2b03633bd6cb2b6
Marc Moreno <ignis.fatue@gmail.com>
2018-09-20 17:11:40 +0200

    extracted URIExtensions and BitMapExtensions

--- core/src/main/java/com/mobilejazz/kotlin/core/ext/UriExtensions.kt
+++ core/src/main/java/com/mobilejazz/kotlin/core/ext/UriExtensions.kt
@@ -4,75 +4,12 @@ import android.annotation.SuppressLint
 import android.content.ContentUris
 import android.content.Context
 import android.database.Cursor
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.Matrix
-import android.media.ExifInterface
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.provider.DocumentsContract
 import android.provider.MediaStore
-import android.support.annotation.CheckResult
-import android.util.Base64
-import java.io.ByteArrayOutputStream
-import java.io.IOException
-
-/*these extensions are super usefull to deal with devices (like Samsung)
-that rotate images because yes*/
-
-
-fun Uri.toBase64(context: Context): String {
-  return BitmapFactory.decodeStream(context.contentResolver.openInputStream(this))
-      .modifyOrientation(this.getPath(context) ?: "")
-      .encodeBase64()
-}
-
-fun Bitmap.encodeBase64(): String {
-  ByteArrayOutputStream().use {
-    this.compress(Bitmap.CompressFormat.JPEG, 10, it)
-    val b = it.toByteArray()
-    return Base64.encodeToString(b, Base64.DEFAULT)
-  }
-}
-
-@CheckResult
-fun Bitmap.rotate(degrees: Float): Bitmap {
-  val matrix = Matrix()
-  matrix.postRotate(degrees)
-  return Bitmap.createBitmap(this, 0, 0, this.width, this.height, matrix, true)
-}
-
-@CheckResult
-fun Bitmap.flip(horizontal: Boolean, vertical: Boolean): Bitmap {
-  val matrix = Matrix()
-  matrix.preScale(if (horizontal) -1f else 1f, if (vertical) -1f else 1f)
-  return Bitmap.createBitmap(this, 0, 0, this.width, this.height, matrix, true)
-}
-
-@Throws(IOException::class)
-@CheckResult
-fun Bitmap.modifyOrientation(image_absolute_path: String): Bitmap {
-  if (image_absolute_path.isBlank()) {
-    return this
-  }
-  val ei = ExifInterface(image_absolute_path)
-  val orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
-
-  return when (orientation) {
-    ExifInterface.ORIENTATION_ROTATE_90 -> this.rotate(90f)
-
-    ExifInterface.ORIENTATION_ROTATE_180 -> this.rotate(180f)
-
-    ExifInterface.ORIENTATION_ROTATE_270 -> this.rotate(270f)
-
-    ExifInterface.ORIENTATION_FLIP_HORIZONTAL -> this.flip(true, false)
-
-    ExifInterface.ORIENTATION_FLIP_VERTICAL -> this.flip(false, true)
-
-    else -> this
-  }
-}
 
 
 fun Uri.getPath(context: Context): String? {
@@ -127,6 +64,13 @@ fun Uri.getPath(context: Context): String? {
   // MediaStore (and general)
 
   return null
+}
+
+
+fun Uri.toBase64(context: Context): String {
+  return BitmapFactory.decodeStream(context.contentResolver.openInputStream(this))
+      .modifyOrientation(this.getPath(context) ?: "")
+      .encodeBase64()
 }
 
 /**
