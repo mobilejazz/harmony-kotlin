17d6e48bae6c7e59b85d6bd7f428cf931305ac2f
Fran Montiel <furanu@gmail.com>
2019-08-06 11:46:59 +0200

    Add extensions to ByteArray and String.Companion.  - Also renamed extension file to follow the same pattern than the rest.    - ExceptionsExt to ExceptionExtensions

---
+++ core/src/main/java/com/mobilejazz/harmony/kotlin/core/ext/ByteArrayExtensions.kt
@@ -0,0 +1,14 @@
+package com.mobilejazz.harmony.kotlin.core.ext
+
+/**
+ * Transforms the [ByteArray] into a hexadecimal [String]
+ * @return a string with the hexadecimal representation of the ByteArray
+ */
+fun ByteArray.toHexString(): String {
+  val sb = StringBuilder()
+  for (aByte in this) {
+    sb.append(String.format("%02x", aByte))
+  }
+
+  return sb.toString()
+}
