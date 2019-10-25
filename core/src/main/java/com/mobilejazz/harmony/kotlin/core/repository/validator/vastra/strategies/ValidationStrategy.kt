db38c84482a50d6f8906e61e357e04cf30730914
Fran Montiel <furanu@gmail.com>
2019-10-25 10:10:48 +0200

    Making ValidationStrategyDataSource Serializable to allow Entity serialization

--- core/src/main/java/com/mobilejazz/harmony/kotlin/core/repository/validator/vastra/strategies/ValidationStrategy.kt
+++ core/src/main/java/com/mobilejazz/harmony/kotlin/core/repository/validator/vastra/strategies/ValidationStrategy.kt
@@ -1,12 +1,14 @@
 package com.mobilejazz.harmony.kotlin.core.repository.validator.vastra.strategies
 
-public enum class ValidationStrategyResult {
+import java.io.Serializable
+
+enum class ValidationStrategyResult {
   UNKNOWN,
   VALID,
   INVALID
 }
 
-interface ValidationStrategyDataSource
+interface ValidationStrategyDataSource : Serializable
 
 interface ValidationStrategy {
 
