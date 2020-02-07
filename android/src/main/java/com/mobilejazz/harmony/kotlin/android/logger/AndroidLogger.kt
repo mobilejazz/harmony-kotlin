d9a9b1e67a54ba7c172f8c85ac6052b3e499c7d3
Javi G <javier.tb@outlook.com>
2020-02-07 06:57:25 +0100

    default implementation for Android Logger methods

--- android/src/main/java/com/mobilejazz/harmony/kotlin/android/logger/AndroidLogger.kt
+++ android/src/main/java/com/mobilejazz/harmony/kotlin/android/logger/AndroidLogger.kt
@@ -46,31 +46,31 @@ open class AndroidLogger : Logger {
   }
 
   override fun sendIssue(tag: String, message: String) {
-    // Nothing to do
+    log(Logger.Level.DEBUG, tag, message)
   }
 
   override fun setDeviceBoolean(key: String, value: Boolean) {
-    // Nothing to do
+    log(Logger.Level.DEBUG, key, value.toString())
   }
 
   override fun setDeviceString(key: String, value: String) {
-    // Nothing to do
+    log(Logger.Level.DEBUG, key, value)
   }
 
   override fun setDeviceInteger(key: String, value: Int) {
-    // Nothing to do
+    log(Logger.Level.DEBUG, key, value.toString())
   }
 
   override fun setDeviceFloat(key: String, value: Float) {
-    // Nothing to do
+    log(Logger.Level.DEBUG, key, value.toString())
   }
 
   override fun removeDeviceKey(key: String) {
-    // Nothing to do
+    log(Logger.Level.DEBUG, key, "RemoveDeviceKey")
   }
 
   override val deviceIdentifier: String
-    get() = TODO("not implemented")
+    get() = ""
 
   private fun createClassTag(): String? {
     val ignoreClass = listOf(
