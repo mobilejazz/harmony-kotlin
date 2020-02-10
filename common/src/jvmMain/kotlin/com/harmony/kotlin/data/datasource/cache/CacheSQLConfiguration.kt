5979c7eb7c5e4754ba6b76bcadad9519ce681fe5
Rubén Vázquez <rubenvot@gmail.com>
2020-02-10 17:49:44 +0100

    - Compiling for ios

---
+++ common/src/jvmMain/kotlin/com/harmony/kotlin/data/datasource/cache/CacheSQLConfiguration.kt
@@ -0,0 +1,19 @@
+package com.harmony.kotlin.data.datasource.cache
+
+import com.harmony.kotlin.data.datasource.database.CacheDatabase
+import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
+import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver.Companion.IN_MEMORY
+
+actual interface CacheSQLConfiguration {
+    actual fun provideCacheDatabase(databaseName: String): CacheDatabase
+}
+
+class CacheSQLNativeDefaultConfiguration: CacheSQLConfiguration {
+    val driver: SqlDriver = JdbcSqliteDriver(IN_MEMORY)
+
+
+    override fun provideCacheDatabase(databaseName: String): CacheDatabase {
+        return CacheDatabase(driver)
+    }
+}
