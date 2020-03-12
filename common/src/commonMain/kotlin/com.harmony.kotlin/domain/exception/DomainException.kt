b092a42dcdf240190cde27fb9be27ebd305af276
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2020-03-12 19:43:08 +0100

    chore(error): updated optinal message value

--- common/src/commonMain/kotlin/com.harmony.kotlin/domain/exception/DomainException.kt
+++ common/src/commonMain/kotlin/com.harmony.kotlin/domain/exception/DomainException.kt
@@ -1,3 +1,3 @@
 package com.harmony.kotlin.domain.exception
 
-open class DomainException(override val message: String?, override val cause: Throwable?) : Exception(message)
+open class DomainException(override val message: String? = null, override val cause: Throwable? = null) : Exception(message)
