35972a8c7b4439015898444ff4cde30dfc9bd624
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2020-04-16 15:13:26 +0200

    fix(jvm): build.gradle configuration for jvm

--- common/src/jvmMain/kotlin/com/harmony/kotlin/common/WeakReference.kt
+++ common/src/jvmMain/kotlin/com/harmony/kotlin/common/WeakReference.kt
@@ -1,11 +1,3 @@
 package com.harmony.kotlin.common
 
-actual class WeakReference<T : Any> actual constructor(referred: T) {
-  actual fun clear() {
-    TODO("Not yet implemented")
-  }
-
-  actual fun get(): T? {
-    TODO("Not yet implemented")
-  }
-}
+actual typealias WeakReference<T> = java.lang.ref.WeakReference<T>
