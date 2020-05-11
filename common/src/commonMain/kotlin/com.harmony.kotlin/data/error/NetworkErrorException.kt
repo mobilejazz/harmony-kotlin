fa707a11d41081494c4e2d70b7a127a04b799fc5
joselufo <joselufo@joselufo-x1.lan>
2020-05-11 09:32:02 +0200

    chore(model): added null as default value to the NetworkErrorException class

--- common/src/commonMain/kotlin/com.harmony.kotlin/data/error/NetworkErrorException.kt
+++ common/src/commonMain/kotlin/com.harmony.kotlin/data/error/NetworkErrorException.kt
@@ -1,4 +1,4 @@
 package com.harmony.kotlin.data.error
 
-class NetworkErrorException(val statusCode: Int, message: String?, throwable: Throwable?) :
+class NetworkErrorException(val statusCode: Int, message: String?, throwable: Throwable? = null) :
     RuntimeException(message, throwable)
