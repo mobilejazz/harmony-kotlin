c802dfb8097128d35f8e8e091b1caa1070ec873b
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2020-06-18 23:44:44 +0200

    refactor(ktor): base network classes

--- common/src/commonMain/kotlin/com.harmony.kotlin/data/error/ktor/KtorNetworkException.kt
+++ common/src/commonMain/kotlin/com.harmony.kotlin/data/error/ktor/KtorNetworkException.kt
@@ -4,8 +4,8 @@ import com.harmony.kotlin.data.error.NetworkErrorException
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpStatusCode
 
-open class KtorNetworkException(status: Int, httpResponse: HttpResponse) : NetworkErrorException(status, null, null)
+open class KtorNetworkException(status: HttpStatusCode, httpResponse: HttpResponse) : NetworkErrorException(status.value, null, null)
 
-class UnauthorizedKtorNetworkException(httpResponse: HttpResponse) : KtorNetworkException(HttpStatusCode.Unauthorized.value, httpResponse)
+class UnauthorizedKtorNetworkException(httpResponse: HttpResponse) : KtorNetworkException(HttpStatusCode.Unauthorized, httpResponse)
 
 
