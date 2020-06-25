444b989cc7a3304d6abfed8c886b9a8922e92959
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2020-06-25 14:15:33 +0200

    chore(error): first approach to the server error managment. Working in progress

--- common/src/commonMain/kotlin/com.harmony.kotlin/data/datasource/network/ktor/KtorConfig.kt
+++ common/src/commonMain/kotlin/com.harmony.kotlin/data/datasource/network/ktor/KtorConfig.kt
@@ -1,16 +1,27 @@
 package com.harmony.kotlin.data.datasource.network.ktor
 
+import com.harmony.kotlin.common.logger.ConsoleLogger
+import com.harmony.kotlin.common.logger.Logger
 import com.harmony.kotlin.data.datasource.network.DefaultUnauthorizedResolution
 import com.harmony.kotlin.data.datasource.network.UnauthorizedResolution
 import com.harmony.kotlin.data.error.DataNotFoundException
 import com.harmony.kotlin.data.error.ktor.KtorNetworkException
+import com.harmony.kotlin.data.error.ktor.ServerError
 import com.harmony.kotlin.data.error.ktor.UnauthorizedKtorNetworkException
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.features.HttpResponseValidator
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.readText
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.isSuccess
+import io.ktor.utils.io.charsets.Charsets
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
 
-fun HttpClientConfig<*>.configureExceptionErrorMapping(unauthorizedResolution: UnauthorizedResolution = DefaultUnauthorizedResolution) {
+
+fun HttpClientConfig<*>.configureExceptionErrorMapping(unauthorizedResolution: UnauthorizedResolution = DefaultUnauthorizedResolution,
+                                                       json: Json = Json(JsonConfiguration.Stable),
+                                                       logger: Logger = ConsoleLogger()) {
   HttpResponseValidator {
     validateResponse { response ->
       val httpStatus = response.status
@@ -24,10 +35,23 @@ fun HttpClientConfig<*>.configureExceptionErrorMapping(unauthorizedResolution: U
             throw DataNotFoundException()
           }
           else -> {
-            throw KtorNetworkException(httpStatus, response)
+            val error = parseServerError(json, logger, response)
+            throw KtorNetworkException(httpStatus, error)
           }
         }
       }
     }
   }
 }
+
+
+internal suspend fun parseServerError(json: Json, logger: Logger, response: HttpResponse): ServerError {
+  return try {
+    val content = response.readText(Charsets.UTF_8)
+    json.parse(ServerError.serializer(), content)
+  } catch (e: Exception) {
+    logger.e(tag = "Network", message = "Error parsing the error body message: $e")
+    ServerError.UnknownServerError
+  }
+}
+
--- common/src/commonMain/kotlin/com.harmony.kotlin/data/error/ktor/KtorNetworkException.kt
+++ common/src/commonMain/kotlin/com.harmony.kotlin/data/error/ktor/KtorNetworkException.kt
@@ -3,9 +3,21 @@ package com.harmony.kotlin.data.error.ktor
 import com.harmony.kotlin.data.error.NetworkErrorException
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpStatusCode
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
-open class KtorNetworkException(val status: HttpStatusCode, val httpResponse: HttpResponse) : NetworkErrorException(status.value, null, null)
 
-class UnauthorizedKtorNetworkException(httpResponse: HttpResponse, isResolved: Boolean) : KtorNetworkException(HttpStatusCode.Unauthorized, httpResponse)
+@Serializable
+data class ServerError(@SerialName("statusCode") val code: Int, val name: String, val message: String) {
+  companion object {
+    val UnknownServerError = ServerError(-1, "Server Error", "Unknown Error")
+    val UnauthorizedServerError = ServerError(1004, "Server Error", "Unauthorized user")
+  }
+}
+
+
+open class KtorNetworkException(val status: HttpStatusCode, val error: ServerError) : NetworkErrorException(status.value, error.message, null)
+
+class UnauthorizedKtorNetworkException(httpResponse: HttpResponse, isResolved: Boolean) : KtorNetworkException(HttpStatusCode.Unauthorized, ServerError.UnauthorizedServerError)
 
 
