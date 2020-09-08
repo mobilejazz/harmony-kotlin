91447478335fa59ee98d6971701bf41667fe5152
Fran Montiel <montiel@mobilejazz.com>
2020-09-08 13:34:55 +0200

    Add try-catch helper functions  - tryOrThrow: Intended for just logging the exception without interfering with it  - tryOrCatch: Intended for ignoring any Exception, logging is optional.  - tryOrNull: Return null if there is any exception (same behavior of the Swift equivalent), logging is optional.

---
+++ common/src/commonMain/kotlin/com.harmony.kotlin/common/exceptions/TryCatchHelpers.kt
@@ -0,0 +1,46 @@
+package com.harmony.kotlin.common.exceptions
+
+import com.harmony.kotlin.common.logger.Logger
+
+/**
+ * Try this block and re-throw any Exception while logging it.
+ */
+inline fun <R> tryOrThrow(logger: Logger, tag: String, message: String = "Exception logged", level: Logger.Level = Logger.Level
+    .WARNING, block: () -> R):
+    R {
+  return try {
+    block()
+  } catch (e: Exception) {
+    logger.log(level, e, tag, message)
+    throw e
+  }
+}
+
+/**
+ * Try this block and catch any Exception.
+ *
+ * Optionally a logger and some parameters can be passed to log the Exception
+ */
+inline fun <R> tryOrCatch(logger: Logger? = null, tag: String = "TryOrCatch", message: String = "Exception logged", level: Logger.Level = Logger.Level.WARNING, block: () -> R) {
+  return try {
+    block()
+    Unit
+  } catch (e: Exception) {
+    logger?.log(level, e, tag, message)
+    Unit
+  }
+}
+
+/**
+ * Try this block and return null if there is any Exception.
+ *
+ * Optionally a logger and some parameters can be passed to log the Exception
+ */
+inline fun <R> tryOrNull(logger: Logger? = null, tag: String = "TryOrNull", message: String = "Exception logged", level: Logger.Level = Logger.Level.WARNING, block: () -> R): R? {
+  return try {
+    block()
+  } catch (e: Exception) {
+    logger?.log(level, e, tag, message)
+    null
+  }
+}
