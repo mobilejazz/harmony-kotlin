45b4e66713f564cc97917e2e557880aa0d3ed93e
Fran Montiel <montiel@mobilejazz.com>
2020-10-13 19:03:52 +0200

    Fix issue making cache to not be refreshed (2)  - Add code to fix invalid cached objects with lastUpdate == null  - Add javadoc stating that TimestampValidationStrategyDataSource lastUpdate should be manually set if instance is build using gson parsing.

--- core/src/main/java/com/mobilejazz/harmony/kotlin/core/repository/validator/vastra/strategies/timestamp/TimestampValidationStrategy.kt
+++ core/src/main/java/com/mobilejazz/harmony/kotlin/core/repository/validator/vastra/strategies/timestamp/TimestampValidationStrategy.kt
@@ -5,7 +5,12 @@ import com.mobilejazz.harmony.kotlin.core.repository.validator.vastra.strategies
 import com.mobilejazz.harmony.kotlin.core.repository.validator.vastra.strategies.ValidationStrategyResult
 import java.util.*
 
-abstract class TimestampValidationStrategyDataSource(open var lastUpdate: Date = Date()) :
+/**
+ * Inherit from this class to provide support for expire date validation when using CacheRepository
+ *
+ * IMPORTANT: When inheriting from this class and creating a new instance parsing from gson you need to manually set the value of lastUpdate field.
+ */
+abstract class TimestampValidationStrategyDataSource(open var lastUpdate: Date) :
     ValidationStrategyDataSource {
 
   abstract fun expiryTime(): Long
@@ -16,7 +21,13 @@ class TimestampValidationStrategy : ValidationStrategy {
   override fun <T : ValidationStrategyDataSource> isValid(t: T): ValidationStrategyResult {
     when (t) {
       is TimestampValidationStrategyDataSource -> {
-        val diff = System.currentTimeMillis() - t.lastUpdate.time
+        val diff = try {
+          System.currentTimeMillis() - t.lastUpdate.time
+        } catch (e: Exception) {
+          // This is a workaround to fix a previous issue that was causing lastUpdate to not be persisted
+          // Now if this is detected the cache will be invalidated and the object will be refreshed from main data source.
+          return ValidationStrategyResult.INVALID
+        }
 
         return if (diff > t.expiryTime())
           ValidationStrategyResult.INVALID
