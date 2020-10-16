02a7ee7110a5159da2268522459e270927141b4a
Fran Montiel <montiel@mobilejazz.com>
2020-10-16 13:09:03 +0200

    MappingRepositoryException & NetworkErrorException inherits now from RepositoryException  - NetworkErrorException has been moved to the Error.kt file in the same package

--- common/src/commonMain/kotlin/com.harmony.kotlin/data/error/Error.kt
+++ common/src/commonMain/kotlin/com.harmony.kotlin/data/error/Error.kt
@@ -1,7 +1,6 @@
 package com.harmony.kotlin.data.error
 
 // Errors
-
 open class RepositoryException(message: String? = null, cause: Throwable? = null) : Exception(message, cause)
 
 open class DataNotFoundException(message: String? = "Data not found", cause: Throwable? = null) : RepositoryException(message, cause)
@@ -14,6 +13,8 @@ class ObjectNotValidException(message: String? = "Object not valid", cause: Thro
 
 class QueryNotSupportedException(message: String? = "Query not supported", cause: Throwable? = null) : RepositoryException(message, cause)
 
-open class MappingException(message: String? = "Exception thrown during mapping", cause: Throwable? = null) : Exception(message, cause)
+open class MappingException(message: String? = "Exception thrown during mapping", cause: Throwable? = null) : RepositoryException(message, cause)
 
-open class SerializationException(message: String? = "Exception thrown during serialization", cause: Throwable? = null) : MappingException(message, cause)
+class SerializationException(message: String? = "Exception thrown during serialization", cause: Throwable? = null) : MappingException(message, cause)
+
+open class NetworkErrorException(val statusCode: Int, message: String?, throwable: Throwable? = null) : RepositoryException(message, throwable)
--- common/src/commonMain/kotlin/com.harmony.kotlin/data/error/NetworkErrorException.kt
+++
@@ -1,4 +0,0 @@
-package com.harmony.kotlin.data.error
-
-open class NetworkErrorException(val statusCode: Int, message: String?, throwable: Throwable? = null) :
-    RuntimeException(message, throwable)
