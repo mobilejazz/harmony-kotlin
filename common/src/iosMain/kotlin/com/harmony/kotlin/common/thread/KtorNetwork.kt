166727944e257359cec59f9c30075001caf3e57f
Rubén Vázquez <rubenvot@gmail.com>
2020-11-06 17:37:26 +0100

    compiles but crashes on ios

--- common/src/iosMain/kotlin/com/harmony/kotlin/common/thread/KtorNetwork.kt
+++ common/src/iosMain/kotlin/com/harmony/kotlin/common/thread/KtorNetwork.kt
@@ -2,21 +2,27 @@ package com.harmony.kotlin.common.thread
 
 import co.touchlab.stately.concurrency.GuardedStableRef
 import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
-import platform.Foundation.NSThread
 import kotlinx.coroutines.Dispatchers
+import platform.Foundation.NSThread
+import kotlin.coroutines.CoroutineContext
 
 actual suspend fun <R> network(block: suspend () -> R): R = coroutineScope {
   withContext(childContext()) {
-    // if (!isMainThread) error("Ktor calls must be run in main thread")
+     if (!isMainThread) error("Ktor calls must be run in main thread")
     // This is a patch to prevent a crash thrown by ktor https://github.com/ktorio/ktor/issues/1165
     // This issue is now solved but still not released. In the future should not be necessary
-    try {
-      block()
-    } catch (t:Throwable) {
+     try {
+       block()
+    } catch (t: Throwable) {
       throw Exception(t)
     }
   }
+
+//  try {
+//    return block()
+//  } catch (t:Throwable) {
+//    throw Exception(t)
+//  }
 }
 
 @OptIn(InternalCoroutinesApi::class)
