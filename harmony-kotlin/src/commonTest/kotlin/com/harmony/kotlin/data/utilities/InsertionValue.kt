e561436f448b52513ffb3dccafcdcf06c38411a0
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2021-11-20 09:46:35 +0100

    chore(tests): added InsertionValues object for the insertion in the putAll() functions

--- harmony-kotlin/src/commonTest/kotlin/com/harmony/kotlin/data/utilities/InsertionValue.kt
+++ harmony-kotlin/src/commonTest/kotlin/com/harmony/kotlin/data/utilities/InsertionValue.kt
@@ -3,3 +3,5 @@ package com.harmony.kotlin.data.utilities
 import com.harmony.kotlin.data.query.KeyQuery
 
 data class InsertionValue<T>(val query: KeyQuery, val value: T?)
+
+data class InsertionValues<T>(val query: KeyQuery, val value: List<T>)
--- harmony-kotlin/src/commonTest/kotlin/com/harmony/kotlin/data/utilities/InsertionValueObjectMother.kt
+++ harmony-kotlin/src/commonTest/kotlin/com/harmony/kotlin/data/utilities/InsertionValueObjectMother.kt
@@ -1,6 +1,13 @@
 package com.harmony.kotlin.data.utilities
 
+import com.harmony.kotlin.common.randomInt
 import com.harmony.kotlin.common.randomString
 import com.harmony.kotlin.data.query.anyKeyQuery
 
 fun anyInsertionValue() = InsertionValue(anyKeyQuery(randomString()), randomString())
+
+fun anyInsertionValues(): InsertionValues<String> {
+  val elements = randomInt(0, 10)
+  val randomValues = (0..elements).map { randomString() }.toList()
+  return InsertionValues(anyKeyQuery(randomString()), randomValues)
+}
