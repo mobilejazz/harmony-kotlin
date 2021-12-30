054025a600d8bf8006005692f55a42912b250e5a
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2021-12-30 14:37:58 +0100

    fix: added the native-mt flag to the kotlin coroutines

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,7 +5,7 @@ const val kotlin_version = "1.6.10"
 
 const val ktor_version = "1.6.7"
 const val serialization_version = "1.3.2"
-const val coroutines_version = "1.6.0"
+const val coroutines_version = "1.6.0-native-mt"
 const val sql_delight_version = "1.5.3"
 const val kotlinx_datetime_version = "0.3.1"
 const val uuid_lib_version = "0.3.1"
