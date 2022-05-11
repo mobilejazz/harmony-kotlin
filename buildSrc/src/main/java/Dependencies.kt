701f060f9d447f16e4afad7e7db4b7588b22e09f
Fran Montiel <montiel@mobilejazz.com>
2022-05-11 14:45:39 +0200

    Update dependencies (#86)
    * Update dependencies
    
    * Update dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,13 +1,13 @@
 const val android_min_sdk_version = 16
 const val android_target_sdk_version = 31
 
-const val kotlin_version = "1.6.10"
+const val kotlin_version = "1.6.21"
 
-const val ktor_version = "1.6.7"
+const val ktor_version = "1.6.8"
 const val serialization_version = "1.3.2"
-const val coroutines_version = "1.6.0-native-mt"
+const val coroutines_version = "1.6.1-native-mt"
 const val sql_delight_version = "1.5.3"
 const val kotlinx_datetime_version = "0.3.2"
 const val uuid_lib_version = "0.3.1"
-const val mockmp_version = "1.2.0"
+const val mockmp_version = "1.5.0"
 const val mockk_version = "1.12.3"
