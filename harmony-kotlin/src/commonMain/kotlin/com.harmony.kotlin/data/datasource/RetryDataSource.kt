83b1faf442656c6d943d4cd4a419eedc620c6ba8
Miguel Torres <migueltorcha@users.noreply.github.com>
2022-07-11 11:01:19 +0200

    KMM-7926 :: Feature internal :: Create retry data source

---
+++ harmony-kotlin/src/commonMain/kotlin/com.harmony.kotlin/data/datasource/RetryDataSource.kt
@@ -0,0 +1,54 @@
+package com.harmony.kotlin.data.datasource
+
+import com.harmony.kotlin.common.logger.Logger
+import com.harmony.kotlin.data.query.Query
+
+class RetryDataSource<V>(
+  private val getDataSource: GetDataSource<V>,
+  private val putDataSource: PutDataSource<V>,
+  private val deleteDataSource: DeleteDataSource,
+  private val maxAmountOfExecutions: Int = 2,
+  private val retryIf: (Exception) -> Boolean = { true },
+  private val logger: Logger? = null,
+) : GetDataSource<V>, PutDataSource<V>, DeleteDataSource {
+
+  init {
+    check(maxAmountOfExecutions > 0)
+  }
+
+  override suspend fun get(query: Query): V =
+    executeWithRetries(retryCount = maxAmountOfExecutions) {
+      getDataSource.get(query)
+    }
+
+  override suspend fun getAll(query: Query): List<V> =
+    executeWithRetries(retryCount = maxAmountOfExecutions) {
+      getDataSource.getAll(query)
+    }
+
+  override suspend fun put(query: Query, value: V?): V =
+    executeWithRetries(retryCount = maxAmountOfExecutions) {
+      putDataSource.put(query, value)
+    }
+
+  override suspend fun putAll(query: Query, value: List<V>?): List<V> =
+    executeWithRetries(retryCount = maxAmountOfExecutions) {
+      putDataSource.putAll(query, value)
+    }
+
+  override suspend fun delete(query: Query) =
+    executeWithRetries(retryCount = maxAmountOfExecutions) {
+      deleteDataSource.delete(query)
+    }
+
+  private suspend fun <K> executeWithRetries(retryCount: Int, functionToRetry: suspend () -> K): K =
+    @Suppress("TooGenericExceptionCaught")
+    try {
+      functionToRetry()
+    } catch (error: Exception) {
+      logger?.d("RetryDataSource", "failed attempt ${this.maxAmountOfExecutions - retryCount + 1} of max retries ${this.maxAmountOfExecutions}:\n\t$error")
+      if (retryCount > 1 && retryIf(error)) {
+        executeWithRetries(retryCount - 1, functionToRetry)
+      } else throw error
+    }
+}
---
+++ harmony-kotlin/src/commonTest/kotlin/com/harmony/kotlin/data/datasource/RetryDataSourceTest.kt
@@ -0,0 +1,133 @@
+package com.harmony.kotlin.data.datasource
+
+import com.harmony.kotlin.common.ANY_ITEMS_COUNT
+import com.harmony.kotlin.common.BaseTest
+import com.harmony.kotlin.common.getSome
+import com.harmony.kotlin.common.logger.ConsoleLogger
+import com.harmony.kotlin.common.randomInt
+import com.harmony.kotlin.common.randomNullable
+import com.harmony.kotlin.common.randomString
+import com.harmony.kotlin.data.query.VoidQuery
+import com.harmony.kotlin.error.DataNotFoundException
+import org.kodein.mock.Mocker
+import org.kodein.mock.UsesMocks
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+@UsesMocks(GetDataSource::class, PutDataSource::class, DeleteDataSource::class)
+class RetryDataSourceTest : BaseTest() {
+  private lateinit var getDataSource: GetDataSource<String>
+  private lateinit var putDataSource: PutDataSource<String>
+  private lateinit var deleteDataSource: DeleteDataSource
+  private val mocker = Mocker()
+
+  @BeforeTest
+  fun beforeTests() {
+    mocker.reset()
+    getDataSource = MockGetDataSource(mocker)
+    putDataSource = MockPutDataSource(mocker)
+    deleteDataSource = MockDeleteDataSource(mocker)
+  }
+
+  @Test
+  fun `should not allow negative amount of executions`() {
+    var exception: IllegalStateException? = null
+
+    try {
+      RetryDataSource(
+        getDataSource, VoidDataSource(), VoidDataSource<Unit>(),
+        maxAmountOfExecutions = randomInt(max = 0)
+      )
+    } catch (illegalStateException: IllegalStateException) {
+      exception = illegalStateException
+    }
+
+    assertNotNull(exception)
+  }
+
+  @Test
+  fun `should work when request works at first attempt`() = runTest {
+    val expectedResponse = getSome { randomString() }
+    var callCounter = 0
+    mocker.everySuspending { getDataSource.getAll(isAny()) } runs {
+      callCounter++
+      expectedResponse
+    }
+    val retryDataSource = RetryDataSource(getDataSource, VoidDataSource(), VoidDataSource<Unit>(), maxAmountOfExecutions = ANY_ITEMS_COUNT)
+
+    val actualResult = retryDataSource.getAll(VoidQuery)
+
+    assertEquals(actualResult, expectedResponse)
+    assertEquals(1, callCounter)
+  }
+
+  @Test
+  fun `should work when request works at second attempt`() = runTest {
+    val expectedResponse = getSome { randomString() }
+    var callCounter = 0
+    mocker.everySuspending { getDataSource.getAll(isAny()) } runs {
+      callCounter++
+      if (callCounter == 1) throw DataNotFoundException()
+      else expectedResponse
+    }
+    val retryDataSource = RetryDataSource(getDataSource, VoidDataSource(), VoidDataSource<Unit>(), maxAmountOfExecutions = ANY_ITEMS_COUNT)
+
+    val actualResult = retryDataSource.getAll(VoidQuery)
+
+    assertEquals(actualResult, expectedResponse)
+    assertEquals(2, callCounter)
+  }
+
+  @Test
+  fun `should fail when all retries failed`() = runTest {
+    val dataException = DataNotFoundException("dummy error")
+    mocker.everySuspending { putDataSource.put(isAny(), isAny()) } runs { throw dataException }
+    val retryDataSource = RetryDataSource(
+      VoidDataSource(),
+      putDataSource,
+      VoidDataSource<Unit>(),
+      maxAmountOfExecutions = ANY_ITEMS_COUNT,
+      logger = ConsoleLogger()
+    )
+    var catchException: Exception? = null
+
+    try {
+      retryDataSource.put(VoidQuery, randomNullable { randomString() })
+    } catch (e: Exception) {
+      catchException = e
+    }
+
+    assertEquals(catchException, dataException)
+  }
+
+  @Test
+  fun `should retry only with expected error`() = runTest {
+    val dataException = DataNotFoundException("dummy error")
+    var callCounter = 0
+    mocker.everySuspending {
+      deleteDataSource.delete(isAny())
+    } runs {
+      callCounter++
+      if (callCounter == 1) throw Exception("first exception")
+      else throw dataException
+    }
+    val retryDataSource = RetryDataSource<String>(
+      VoidDataSource(), VoidDataSource(),
+      deleteDataSource,
+      maxAmountOfExecutions = Int.MAX_VALUE,
+      retryIf = { it !is DataNotFoundException },
+      logger = ConsoleLogger()
+    )
+    var catchException: Exception? = null
+
+    try {
+      retryDataSource.delete(VoidQuery)
+    } catch (exception: Exception) {
+      catchException = exception
+    }
+    assertEquals(catchException, dataException)
+    assertEquals(2, callCounter)
+  }
+}
