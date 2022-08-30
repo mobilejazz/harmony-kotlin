1474c8f84eed0c8c6a68e6626586a8faf08de5ab
Fran Montiel <montiel@mobilejazz.com>
2022-08-30 11:04:49 +0200

    KMM-4066 :: Feature :: Minor fixes

--- sample-core/src/commonMain/kotlin/com/mobilejazz/kmmsample/core/feature/hackerposts/domain/interactor/GetHackerNewsPostInteractor.kt
+++ sample-core/src/commonMain/kotlin/com/mobilejazz/kmmsample/core/feature/hackerposts/domain/interactor/GetHackerNewsPostInteractor.kt
@@ -5,12 +5,17 @@ import com.harmony.kotlin.domain.interactor.either.GetInteractor
 import com.harmony.kotlin.error.HarmonyException
 import com.mobilejazz.kmmsample.core.feature.hackerposts.domain.HackerNewsQuery
 import com.mobilejazz.kmmsample.core.feature.hackerposts.domain.model.HackerNewsPost
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 
 class GetHackerNewsPostInteractor(
+  private val coroutineContext: CoroutineContext,
   private val getHackerNewsPostInteractor: GetInteractor<HackerNewsPost>
 ) {
 
   suspend operator fun invoke(hackerNewsPostId: Long): Either<HarmonyException, HackerNewsPost> {
-    return getHackerNewsPostInteractor(HackerNewsQuery.GetPost(hackerNewsPostId))
+    return withContext(coroutineContext) {
+      getHackerNewsPostInteractor(HackerNewsQuery.GetPost(hackerNewsPostId))
+    }
   }
 }
